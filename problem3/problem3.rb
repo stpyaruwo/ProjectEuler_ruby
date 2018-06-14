07ad05a7c6b1a44e3a62a640367010784ef12697
yeatea <harison.4663@gmail.com>
2018-06-14 19:21:00 +0900

    problem3 修正

--- problem3/problem3.rb
+++ problem3/problem3.rb
@@ -1,14 +1,5 @@
+require "prime"
 
-factors = 600851475143
-factors2 = factors / 2
-max_divid = 1
+factor = Prime.prime_division(600851475143)
 
-#時間がかかり過ぎるので、要検討
-factors2.downto(0) do |i|
-  if factors %i == 0
-    max_divid = i
-    break
-  end
-end
-
-puts max_divid
+puts factor[-1][0]
